ba9f5a88ec07c6c29282a90f846293165cce50a6
kavinesh <kavineshkavinesh860@gmail.com>
2024-04-17 22:20:29 +0530

    Note Taker

--- Note_Taker/script.js
+++ Note_Taker/script.js
@@ -1,72 +1,85 @@
-const addBtn = document.querySelector("#addBtn");
-const mainDiv = document.querySelector("#main");
+/**
+ * What am i trying to achieve?
+     To create a div that accepts title and notes.
+
+  Dividing the problem into sub-problem:
+     1. select the required elements and store it in the variable
+     2. create a function to add div that has the main content like title, notes, delete button, save button.
+     3. create a function for delete button that deletes existing div.
+     4. create a function for save button that accepts the saveNote function 
+     5. create a function for saving the created div and its contents in the localstorage.
+        select the content div and select the title div and its value also.
+     6. create a function for getting the saved item from the localstorage.
+     create a addEventListener for Addnote, DeleteBtn, and SaveBtn.
+
+*/
+const addBtn = document.getElementById("addBtn");
+const mainDiv = document.getElementById("main");
 
 function saveNote() {
   const title = document.querySelectorAll(".note .title");
   const content = document.querySelectorAll(".note .content");
-
   const data = [];
-
   content.forEach((element, index) => {
-    const contentVal = element.value;
-    const titleVal = title[index].value;
-    if (contentVal.trim() !== "") {
-      data.push({ titleVal, contentVal });
+    const elItem = element.value.trim();
+    const titleItem = title[index].value.trim();
+    if (elItem.trim() !== "") {
+      data.push({ titleItem, elItem });
     }
   });
 
-  const titleData = data.map((val) => val.titleVal);
-  localStorage.setItem("title", JSON.stringify(titleData));
-
-  const contentData = data.map((val) => val.contentVal);
-  localStorage.setItem("content", JSON.stringify(contentData));
+  const saveData = JSON.stringify(data);
+  localStorage.setItem("notes", saveData);
 }
 
-function addNote(e, text = "", title = "") {
-  //   e.preventDefault();
+function getData() {
+  const parsedData = JSON.parse(localStorage.getItem("notes"));
+  if (parsedData && parsedData[0]) {
+    parsedData.forEach((_, index) => {
+      const loadedContent = parsedData[index]?.elItem;
+      const loadedTitle = parsedData[index]?.titleItem;
+      addNote(loadedContent, loadedTitle);
+    });
+  }
+}
 
+function addNote(text = "", title = "") {
+  // console.log(title);
   const noteDiv = document.createElement("div");
-
   noteDiv.classList.add("note");
-  noteDiv.innerHTML = `
+  noteDiv.insertAdjacentHTML(
+    "afterbegin",
+    `
 	<div class="icons">
-		<i class="save fas fa-save"
-			style="color:red">
-		</i>
-		<i class="trash fas fa-trash"
-			style="color:yellow">
-		</i>
+		<i class="save fas fa-save" style="color:red"></i>
+		<i class="trash fas fa-trash" style="color:yellow"></i>
 	</div>
 	<div class="title-div">
-		<textarea class="title"
-			placeholder="Write the title ...">${title}
-		</textarea>
+		<textarea class="title" placeholder="Write the title ...">${title}</textarea>
 	</div>
-	<textarea class="content"
-		placeholder="Note down your thoughts ...">${text}
-	</textarea>
-	`;
-  //   console.log(document.querySelector(".note .content"));
+	<textarea class="content" placeholder="Note down your thoughts ...">${text}</textarea>
+	`
+  );
+  mainDiv.appendChild(noteDiv);
 
   function deleteItem() {
     noteDiv.remove();
     saveNote();
   }
 
+  const saveBtn = noteDiv.querySelector(".save");
+  const deleteBtn = noteDiv.querySelector(".trash");
+
   function saveItem() {
     saveNote();
   }
-  const deleteBtn = noteDiv.querySelector(".trash");
-  const saveBtn = noteDiv.querySelector(".save");
 
   saveBtn.addEventListener("click", saveItem);
   deleteBtn.addEventListener("click", deleteItem);
-  mainDiv.appendChild(noteDiv);
-  saveNote();
 }
-// function getNote() {
-//   const  = localStorage.getItem("Item");
 
-// }
-addBtn.addEventListener("click", addNote.bind());
-// getNote();
+addBtn.addEventListener("click", function () {
+  addNote();
+});
+
+getData();
