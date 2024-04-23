9bb780f14d292d14ddd20bd16d65d1c82fc7ee97
kavinesh <kavineshkavinesh860@gmail.com>
2024-04-23 22:35:43 +0530

    Age calculator

--- Age_calculator/script.js
+++ Age_calculator/script.js
@@ -1,6 +1,7 @@
 const dobInput = document.getElementById("inputDob");
 const currentInput = document.getElementById("cdate");
 const calculateBtn = document.querySelector("button");
+const ageContainer = document.getElementById("currentAge");
 
 function getDOB(dob, current) {
   const dobDate = new Date(dob);
@@ -32,10 +33,26 @@ function getDOB(dob, current) {
     m3--;
     d3 = getDate(y1, m1) + d2 - d1;
   }
+  ageContainer.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+  currentInput.setAttribute(
+    "value",
+    `${y2.toString()}-${m2.toString().padStart(2, "0")}-${d2.toString()}`
+  );
 }
+
+// function showCurDate(y2, m2, d2) {
+
+// }
 
 function getDate(year, month) {
   return new Date(year, month, 0).getDate();
 }
 
-getDOB("06/08/2004", "04/23/2024");
+function calculateAge() {
+  const dobValue = dobInput.value;
+  const curValue = currentInput.value;
+  getDOB(dobValue, curValue);
+}
+
+// calculateBtn.addEventListener("click", calculateAge);
+calculateAge();
