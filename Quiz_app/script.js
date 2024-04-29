c531cc285798e86fefc2655712d607e68c5c8cd0
kavinesh <kavineshkavinesh860@gmail.com>
2024-04-29 21:39:06 +0530

    Quiz game

---
+++ Quiz_app/script.js
@@ -0,0 +1,6 @@
+const api =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
+const questions = document.querySelector(".question");
+const optionSection = document.querySelector(".options");
+const submitBtn = document.querySelector("#btn");
+const scoresSection = document.querySelector("#score");
